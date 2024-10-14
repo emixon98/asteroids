d80f4048af29dac506abdfe08faca6fdc3e066f2
emixon98 <emixon98@gmail.com>
2024-10-13 22:41:53 -0700

    Tired, going to bed

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import pygame
 from constants import *
 
 def main():
+    
     print("Starting asteroids!")
     print("Screen width: {}".format(SCREEN_WIDTH))
     print("Screen height: {}".format(SCREEN_HEIGHT))
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    
     
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
